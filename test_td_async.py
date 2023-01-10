e61251bf06b6333cb22d15f11e8d4768f2f0d7bf
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-01-10 22:44:33 +0000

    Added async concurrent download of files + decoding example

--- test_td_async.py
+++ test_td_async.py
@@ -1,5 +1,7 @@
-import os
+import asyncio
+import fsspec
 import json
+import os
 import time
 import torch
 import numpy as np
@@ -9,28 +11,42 @@ from torchdata.dataloader2 import DataLoader2, MultiProcessingReadingService
 from torchvision.io import decode_jpeg
 
 CONNECTION_STRING=os.environ['CONNECTION_STRING']
+storage_options={'connection_string': CONNECTION_STRING}
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-def decode(d):
-    # byte_tensor = torch.frombuffer(d[1], dtype=torch.uint8)
-    # return decode_jpeg(byte_tensor)
-    torch.frombuffer(d[1], dtype=torch.uint8).clone()
-    return torch.zeros([3,10,10])
+def open_read(batch):
+    fs = fsspec.filesystem("abfs", **storage_options)
+    fh = fs.cat(batch)
 
+    return fh
+
+async def create_tensor(buffer):
+    return torch.frombuffer(buffer, dtype=torch.uint8).clone()
+
+async def do_work(buffer):
+    return decode_jpeg(torch.frombuffer(buffer, dtype=torch.uint8))
+    # return torch.frombuffer(buffer, dtype=torch.uint8).clone()
+
+async def decode_async(batch):
+    images = [await do_work(b) for _, b in batch.items()]
+    return images
+
+def decode(batch):
+    images = asyncio.run(decode_async(batch))
+    return images
+    # return [torch.zeros([3,10,10]) for _ in range(len(batch))]
 
 def run(batch_size, num_workers):
     with open('file_list.json', 'r') as f:
         files = [f'abfs://{a}' for a in json.load(f)]
 
     storage_options={'connection_string': CONNECTION_STRING}
-    # dp = IterableWrapper(['abfs://bdd100k/images/100k/train']).list_files_by_fsspec(**storage_options).sharding_filter().open_files_by_fsspec(mode='rb',**storage_options)
-
-    dp = IterableWrapper(files).sharding_filter().open_files_by_fsspec(mode='rb',**storage_options)
-    dp = dp.read_from_stream().map(fn=decode).batch(batch_size).collate()
+    dp = IterableWrapper(files).sharding_filter().batch(batch_size).map(fn=open_read)
+    
+    dp = dp.map(fn=decode).collate()
 
     dl = DataLoader2(datapipe=dp, reading_service=MultiProcessingReadingService(num_workers=num_workers))
-    # dl = DataLoader(dp, batch_size=None, num_workers=num_workers)
 
     batch_times=[]
 
@@ -57,4 +73,4 @@ def main(batch_size=64, num_workers=(24,48, 64, 128), num_runs=5):
     print({k: np.mean(v) for k,v in results.items()})
 
 if __name__ == '__main__':
-    main(batch_size=32,  num_workers=[32], num_runs=5)
+    main(batch_size=64,  num_workers=[64], num_runs=5)
